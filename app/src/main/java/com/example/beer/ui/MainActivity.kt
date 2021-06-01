30d540c8797f825a0cca87666de245aa27935a47
marko <markokatic0001@gmail.com>
2021-06-01 22:36:34 +0200

    Added app style

--- app/src/main/java/com/example/beer/ui/MainActivity.kt
+++ app/src/main/java/com/example/beer/ui/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.beer.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.beer.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navController = findNavController(R.id.navigationFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
     }
 }
