9e320fad12121a3bc78935ffdaebccd09fdd6d48
marko <markokatic0001@gmail.com>
2021-06-02 19:31:44 +0200

    Added DB queries to sort beers by abv, ibu and ebc parameters.

--- app/src/main/java/com/example/beer/room/BeersDao.kt
+++ app/src/main/java/com/example/beer/room/BeersDao.kt
@@ -10,4 +10,13 @@ interface BeersDao {
 
     @Query("SELECT * FROM beers")
     fun beerList(): LiveData<List<BeerDB>?>
+
+    @Query("SELECT * FROM beers ORDER BY abv ASC")
+    fun getBeersByAbv(compRef: String?): List<BeerDB>?
+
+    @Query("SELECT * FROM beers ORDER BY ibu ASC")
+    fun getBeersByIbu(compRef: String?): List<BeerDB>?
+
+    @Query("SELECT * FROM beers ORDER BY ebc ASC")
+    fun getBeersByEbc(compRef: String?): List<BeerDB>?
 }
